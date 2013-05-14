b8f4e12257c22542ec61a875ea664ef6bbdde0ef
qfang <irisjacky99@yahoo.com.au>
2013-05-14 22:23:35 +1000

    fixed background ptn and added proper effects

--- app/assets/javascripts/highlight.js
+++ app/assets/javascripts/highlight.js
@@ -1,23 +1,32 @@
 $("#highlight_more_btn").click(function() {
-  $("#highlight_minimum_view").fadeOut(500, "linear");
-  $("#highlight_more_btn").fadeOut(500, "linear"); 
-
+  $("#highlight_minimum_view").hide();
+  $("#highlight_more_btn").hide(); 
+  $("#highlight_dtl_header").show();
   $("#highlight_dtl").slideDown("slow");
-
-
 });
 
 
 $("#highlight_close_btn").click(function(){
-  $("#highlight_dtl").slideUp("slow");
-
-  $("#highlight_minimum_view").fadeIn(500, "linear");
-  $("#highlight_more_btn").fadeIn(500, "linear"); 
+	$("#highlight_dtl_header").hide();
+	$("#project_details").slideUp("slow");	
+	$("#highlight_dtl").slideUp("slow");
+    $("#highlight_minimum_view").fadeIn(500, "linear");
+  	$("#highlight_more_btn").fadeIn(800, "linear");
 
 });
 
 $("#project_dtl_btn").click(function(){
 
-  $("#project_dtl_btn").fadeIn(500, "linear"); 
-  $("#project_details").slideDown("slow");
+  	$("#project_dtl_btn").hide();
+	$("#close_project").show();
+  	$("#project_details").slideDown("slow");
+
 });
+
+$("#project_close_btn").click(function(){
+	$("#close_project").hide();
+  	$("#project_dtl_btn").show();
+  	$("#project_details").slideUp("slow");
+
+});
+
